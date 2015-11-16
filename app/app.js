4b695b4ee2649cf8ff2e33e36ee6cd1ff7a789df
Dean Chuang <deanchuang1@gmail.com>
2015-11-16 11:38:57 -0800

    Added smooth scrolling to portfolio

--- app/app.js
+++ app/app.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
   
-
+  // code for smooth scrolling
+  $("a[href='#']").click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+  });
+  
+  $("a[href='#about']").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+  });
+  
+  $("a[href='#work']").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#work").offset().top
+    }, 1000);
+  });
+  
+  $("a[href='#contact']").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+  });
+  
+  $("a[href='#faq']").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#faq").offset().top
+    }, 1000);
+  });
+  
+  // code for content hover
   $('#saas-image').contenthover({
     overlay_background:'#000',
     overlay_opacity:0.8
