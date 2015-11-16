0bb1fdb5523787910fcbfb7a887ca29534ee3284
Dean Chuang <deanchuang1@gmail.com>
2015-11-15 23:15:52 -0800

    added bootstrap navbar with scrollspy

---
+++ app/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  
+
+  $('#saas-image').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+  });
+  $('#pinclone-image').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+  });
+  $('#sf-image').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+  });
+  $('#shop-image').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+  });
+  $('#api-image').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+  });
+  $('#quiz-image').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8
+  });
+});
