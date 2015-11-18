9c9eb0bfa369bfba00e8cfcb5153ba3e586e9ba0
Dean Chuang <deanchuang1@gmail.com>
2015-11-17 17:39:52 -0800

    removed contenthover.js and added my own script

--- app/app.js
+++ app/app.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
   
+  
+  
+  // code for image hover
+  $(".about-image").mouseenter(function(){
+    $(".about-image").fadeOut(800, function(){
+      $(".about-image2").fadeIn(800, function(){
+        $(".about-image2").mouseleave(function(){
+          $(".about-image2").fadeOut(800, function(){
+            $(".about-image").fadeIn(800);
+          });
+        });
+      });
+      });
+    });
+  
+  /*$(".about-image").hover(function(){
+    $(".about-image").fadeOut(800, function(){
+      $(".about-image2").fadeIn(800);
+    });
+  }, function(){
+    $(".about-image2").fadeOut(800, function(){
+       $(".about-image").fadeIn(800);
+    });
+  });*/
+  
+/*  $(".image-box").hover(function(){
+    $(".about-image").hide();
+    $(".about-image2").show();
+  }, function(){
+    $(".about-image2").hide();
+    $(".about-image").show();
+  })*/
+  
   // code for smooth scrolling
   $("a[href='#']").click(function(e){
     e.preventDefault();
@@ -7,15 +40,6 @@ $(document).ready(function(){
         scrollTop: 0
     }, 1000);
   });
-  
-  // code for image hover
-  $(".image-box").mouseover(function(){
-    $(".about-image").hide();
-    $(".about-image2").show();
-  }).mouseout(function(){
-    $(".about-image2").hide();
-    $(".about-image").show();
-  })
   
   $("a[href='#about']").click(function(e) {
     e.preventDefault();
@@ -46,7 +70,44 @@ $(document).ready(function(){
   });
   
   // code for content hover
-  $('#saas-image').contenthover({
+  
+  $('#saas-image').hover(function(){
+    $('#saas-image.content-holder h3').fadeIn(1000);
+  }, function(){
+    $('#saas-image.content-holder h3').fadeOut(1000);
+  });
+  
+  $('#pinclone-image').hover(function(){
+    $('#pinclone-image.content-holder h3').fadeIn(1000);
+  }, function() {
+    $('#pinclone-image.content-holder h3').fadeOut(1000);
+  });
+  
+  $('#sf-image').hover(function(){
+    $('#sf-image.content-holder h3').fadeIn(1000);
+  }, function(){
+    $('#sf-image.content-holder h3').fadeOut(1000);
+  });
+  
+  $('#shop-image').hover(function(){
+    $('#shop-image.content-holder h3').fadeIn(1000);
+  }, function(){
+    $('#shop-image.content-holder h3').fadeOut(1000);
+  });
+  
+  $('#api-image').hover(function(){
+    $('#api-image.content-holder h3').fadeIn(1000);
+  }, function() {
+    $('#api-image.content-holder h3').fadeOut(1000);
+  });
+  
+  $('#quiz-image').hover(function(){
+    $('#quiz-image.content-holder h3').fadeIn(1000);
+  }, function(){
+    $('#quiz-image.content-holder h3').fadeOut(1000);
+  });
+  
+  /*$('#saas-image').contenthover({
     overlay_background:'#000',
     overlay_opacity:0.8
   });
@@ -69,5 +130,5 @@ $(document).ready(function(){
   $('#quiz-image').contenthover({
     overlay_background:'#000',
     overlay_opacity:0.8
-  });
+  });*/
 });
